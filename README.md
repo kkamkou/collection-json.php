0405de4f5eaf0eeb1b81dccef8bb388ab0d09911
Kanstantsin Kamkou <kkamkou@gmail.com>
2013-12-11 12:42:09 +0100

    readme updated

--- README.md
+++ README.md
@@ -5,4 +5,29 @@ PHP implementation of the Collection+JSON [specification](http://amundsen.com/me
 
 Examples of media type in use [can be found here](http://amundsen.com/media-types/collection/examples/).
 
+## Example
+```
+use \CollectionJson\Collection;
+use \CollectionJson\Property;
+
+$data = new Property\Data('firstName', 'Duck', 'Full name');
+$link = new Property\Link('http://example.com', 'homepage', 'Homepage', 'link', 'Link to the homepage');
+
+$item = new Collection\Item('http://example.com/item');
+$item->addData($data)->addLink($link);
+
+$collection = new Collection('http://example.com');
+$collection->addItem($item);
+
+echo $collection;
+```
+
+## Tests
+```
+phpcs --standard=psr2 CollectionJson
+cd tests
+phpunit ./
+```
+
+
 [![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/kkamkou/collection-json.php/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
--- tests/CollectioTest.php
+++ tests/CollectioTest.php
@@ -1,22 +1,18 @@
 <?php
 
 use \CollectionJson\Collection;
-use \CollectionJson\Collection\Template;
-use \CollectionJson\Collection\Error;
-use \CollectionJson\Collection\Item;
-use \CollectionJson\Property\Data;
-use \CollectionJson\Property\Link;
+use \CollectionJson\Property;
 
 class CollectionTest extends PHPUnit_Framework_TestCase
 {
     public function testTemplate()
     {
         $datas = array(
-            new Data('firstName', 'PHPUnit', 'Full name'),
-            new Data('age', 23, 'Ages')
+            new Property\Data('firstName', 'PHPUnit', 'Full name'),
+            new Property\Data('age', 23, 'Ages')
         );
 
-        $template = new Template($datas);
+        $template = new Collection\Template($datas);
 
         $collection = new Collection('http://example.com');
         $collection->setTemplate($template);
@@ -24,10 +20,10 @@ class CollectionTest extends PHPUnit_Framework_TestCase
 
     public function testItems()
     {
-        $data = new Data('firstName', 'PHPUnit', 'Full name');
-        $link = new Link('http://example.com', 'something', 'Strange link', 'link', 'Some link');
+        $data = new Property\Data('firstName', 'PHPUnit', 'Full name');
+        $link = new Property\Link('http://example.com', 'something', 'Strange link', 'link', 'Some link');
 
-        $item = new Item('http://example.com/item');
+        $item = new Collection\Item('http://example.com/item');
         $item->addData($data)->addData($data);
         $item->addLink($link)->addLink($link);
 
@@ -38,13 +34,13 @@ class CollectionTest extends PHPUnit_Framework_TestCase
 
     public function testError()
     {
-        $error = new Error('An error', '0x00000001', 'Blue Screen');
+        $error = new Collection\Error('An error', '0x00000001', 'Blue Screen');
         $collection = new Collection('http://example.com');
         $collection->setError($error);
         $output = json_decode($collection, true);
         $this->assertEquals($output['collection']['error'], $error->__toArray());
 
-        $collection->setError(new Error('An error'));
+        $collection->setError(new Collection\Error('An error'));
         $output = json_decode($collection, true);
         $this->assertEquals($output['collection']['error'], array('title' => 'An error'));
     }
